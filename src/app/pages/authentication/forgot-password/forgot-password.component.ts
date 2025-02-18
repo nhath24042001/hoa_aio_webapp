fd4b015e50bcf39aace02f65b6b49282421dc010
nhath24042001 <huy.nguyen@qkit.vn>
2025-02-18 15:05:01 +0700

    feat(forgot-password): implement forgot password form with validation and styles

--- src/app/pages/authentication/forgot-password/forgot-password.component.ts
+++ src/app/pages/authentication/forgot-password/forgot-password.component.ts
@@ -1,9 +1,30 @@
+import { CustomInputComponent } from './../../../components/shared/custom-input/custom-input.component';
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { ButtonModule } from 'primeng/button';
 
 @Component({
   selector: 'app-forgot-password',
-  imports: [],
+  imports: [ReactiveFormsModule, CustomInputComponent, ButtonModule],
   templateUrl: './forgot-password.component.html',
   styleUrl: './forgot-password.component.scss'
 })
-export class ForgotPasswordComponent {}
+export class ForgotPasswordComponent {
+  forgotPswForm: FormGroup;
+  isSubmitting = false;
+  loading = false;
+
+  constructor(private fb: FormBuilder) {
+    this.forgotPswForm = this.fb.group({
+      email: ['', [Validators.required, Validators.email]]
+    });
+  }
+
+  onSubmit() {
+    this.isSubmitting = true;
+
+    setTimeout(() => {
+      this.loading = false;
+    }, 2000);
+  }
+}
