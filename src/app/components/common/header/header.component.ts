8e8ec743c04212933d6a49e7f53e28a1493282ef
nhath24042001 <huy.nguyen@qkit.vn>
2025-02-20 10:13:55 +0700

    feat(header): enhance header component with dropdown menu and theme toggle functionality

--- src/app/components/common/header/header.component.ts
+++ src/app/components/common/header/header.component.ts
@@ -1,11 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { Popover, PopoverModule } from 'primeng/popover';
+import { CommonModule } from '@angular/common';
+import { MENU_DROPDOWN } from '../../../constants/header';
+import { THEME } from '../../../constants';
 
 @Component({
   selector: 'app-header',
-  imports: [],
+  imports: [PopoverModule, CommonModule],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss'
 })
 export class HeaderComponent {
+  @ViewChild('op') op!: Popover;
 
+  menu_dropdowns = MENU_DROPDOWN;
+  THEME = THEME;
+  currentMode = THEME.LIGHT;
+
+  toggle(event: any) {
+    this.op.toggle(event);
+  }
+
+  toggleTheme() {
+    const element = document.querySelector('html');
+    if (element != null) {
+      element.classList.toggle('my-app-dark');
+    }
+    this.currentMode = this.currentMode === THEME.LIGHT ? THEME.DARK : THEME.LIGHT;
+  }
 }
---
+++ src/app/constants/header.ts
@@ -0,0 +1,32 @@
+export const MENU_DROPDOWN = [
+  {
+    icon: 'account-menu',
+    name: 'My Account',
+    routeLink: '/my-account'
+  },
+  {
+    icon: 'notification-menu',
+    name: 'Notification settings',
+    routeLink: '/notification-settings'
+  },
+  {
+    icon: 'change-password',
+    name: 'Change Password',
+    RouterLink: '/change-password'
+  },
+  {
+    icon: 'term-menu',
+    name: 'Terms & Conditions',
+    routeLink: '/terms-conditions'
+  },
+  {
+    icon: 'about-us',
+    name: 'About Us',
+    routeLink: '/about-us'
+  },
+  {
+    icon: 'contact-us',
+    name: 'Contact Us',
+    routeLink: '/contact-us'
+  }
+]
