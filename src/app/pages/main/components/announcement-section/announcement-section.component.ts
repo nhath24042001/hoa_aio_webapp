ad33143f42dd399225514df8eb2a80426c30feff
nhath24042001 <huy.nguyen@qkit.vn>
2025-02-24 08:53:50 +0700

    feat(table): add table component with basic structure and styles; introduce role enum and update announcement section data

--- src/app/pages/main/components/announcement-section/announcement-section.component.ts
+++ src/app/pages/main/components/announcement-section/announcement-section.component.ts
@@ -9,5 +9,23 @@ import { ButtonPrimaryComponent } from '../shared/button-primary/button-primary.
   styleUrl: './announcement-section.component.scss'
 })
 export class AnnouncementSectionComponent {
-  announcements = [];
+  announcements = [
+    {
+      task_name: 'Sign contract with plumbing vendor',
+      type: 'Maintenance',
+      Priority: 'Urgent',
+      created_at: '2021-07-01',
+      assignTo: [
+        {
+          name: 'John Doe',
+          role: 'Plumber'
+        },
+        {
+          name: 'Jane Smith',
+          role: 'Plumber'
+        }
+      ],
+      status: 'New'
+    }
+  ];
 }
