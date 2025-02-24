b4a9a24dcf90edd61b25d120de57dd120bbc5c24
nhath240401 <nhath24042001@gmail.com>
2025-02-24 20:44:03 +0700

    feat(overview): update overview component imports; remove unused task management component; enhance SCSS variable usage for theming

--- src/app/pages/main/pages/overview/overview.component.ts
+++ src/app/pages/main/pages/overview/overview.component.ts
@@ -1,16 +1,14 @@
 import { Component } from '@angular/core';
 import { BaseComponent } from '../../../../components/common/base/base.component';
 import { OverviewStasComponent } from '~/pages/main/components/modules/overview/overview-stas/overview-stas.component';
-import { TaskManagementComponent } from '../task-management/task-management.component';
 import { TaskSectionComponent } from '~/pages/main/components/modules/overview/task-section/task-section.component';
 import { AnnouncementSectionComponent } from '~/pages/main/components/modules/overview/announcement-section/announcement-section.component';
 import { BidSectionComponent } from '../../components/bid-section/bid-section.component';
-import { CalendarComponent } from '../calendar/calendar.component';
 import { CalendarSectionComponent } from "../../components/modules/overview/calendar-section/calendar-section.component";
 
 @Component({
   selector: 'app-overview',
-  imports: [OverviewStasComponent, TaskManagementComponent, AnnouncementSectionComponent, BidSectionComponent, TaskSectionComponent, CalendarComponent, CalendarSectionComponent],
+  imports: [OverviewStasComponent, AnnouncementSectionComponent, BidSectionComponent, TaskSectionComponent, CalendarSectionComponent],
   templateUrl: './overview.component.html',
   styleUrl: './overview.component.scss'
 })
