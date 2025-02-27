9012d60ac4226af182e18166f7ea07b0b9c1dc75
nhath24042001 <huy.nguyen@qkit.vn>
2025-02-27 17:00:29 +0700

    feat(overview): enhance announcement section with dynamic data and styling

--- src/app/pages/main/components/modules/overview/announcement-section/announcement-section.component.ts
+++ src/app/pages/main/components/modules/overview/announcement-section/announcement-section.component.ts
@@ -1,13 +1,43 @@
 import { Component } from '@angular/core';
 import { EmptyContentComponent } from '../../../shared/empty-content/empty-content.component';
 import { ButtonPrimary } from '../../../shared/button-primary/button-primary.component';
+import { IAnnouncementChild } from '~/@types/announcement';
+import { BaseComponent } from '~/components/common/base/base.component';
+import { ThemeService } from '~/services/theme.service';
+import { DatePipe } from '@angular/common';
 
 @Component({
   selector: 'app-announcement-section',
-  imports: [EmptyContentComponent, ButtonPrimary],
+  imports: [EmptyContentComponent, ButtonPrimary, DatePipe],
   templateUrl: './announcement-section.component.html',
   styleUrl: './announcement-section.component.scss'
 })
-export class AnnouncementSectionComponent {
-  announcements = [];
+export class AnnouncementSectionComponent extends BaseComponent {
+  announcements: IAnnouncementChild[] = [
+    {
+      title: 'Long Announcement Title Can Be Truncated',
+      type: 'Draft',
+      created: '2025-10-16 08:46:00',
+      personSent: 'Kerry Gant'
+    },
+    {
+      title: 'Announcement with Some Content',
+      created: '2025-02-24: 08:46:00',
+      personSent: 'Larry Birch'
+    },
+    {
+      title: 'Long Announcement Title Can Be Truncated',
+      created: '2025-02-24: 08:46:00',
+      personSent: 'Larry Birch'
+    },
+    {
+      title: 'Long Announcement Title Can Be Truncated',
+      created: '2025-02-24: 08:46:00',
+      personSent: 'Larry Birch'
+    }
+  ];
+
+  constructor(themeService: ThemeService) {
+    super(themeService);
+  }
 }
