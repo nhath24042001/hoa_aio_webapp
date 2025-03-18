6a304ca6b539e12f165cad37470ddf01059ffd68
nhath24042001 <huy.nguyen@qkit.vn>
2025-03-18 14:32:30 +0700

    feat(auth): implement AuthService and CryptoService for phone-based authentication and encryption

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,5 @@
+export const environment = {
+  production: true,
+  API_URL: 'https://stg.api.hoaallinone.com/api',
+  SECRET_KEY: 'HSDS'
+};
---
+++ src/environments/environment.ts
@@ -0,0 +1,5 @@
+export const environment = {
+  production: false,
+  API_URL: 'https://stg.api.hoaallinone.com/api',
+  SECRET_KEY: 'HSDS'
+};
