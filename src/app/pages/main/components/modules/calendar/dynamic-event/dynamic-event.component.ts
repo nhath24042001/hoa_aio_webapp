2c9dc389acadd35493b54c149ca9901da64cd022
nhath24042001 <huy.nguyen@qkit.vn>
2025-03-26 10:16:23 +0700

    feat(calendar): update dynamic event component with simplified structure and new calendar-plus SVG icon

--- src/app/pages/main/components/modules/calendar/dynamic-event/dynamic-event.component.ts
+++ src/app/pages/main/components/modules/calendar/dynamic-event/dynamic-event.component.ts
@@ -1,79 +1,41 @@
-import { Component, ElementRef, ViewChild } from '@angular/core';
-import { DividerModule } from 'primeng/divider';
-import { DynamicDialogRef, DynamicDialogConfig } from 'primeng/dynamicdialog';
-import { SelectModule } from 'primeng/select';
-import { FormsModule } from '@angular/forms';
-import { InputTextModule } from 'primeng/inputtext';
-import { DatePickerModule } from 'primeng/datepicker';
-import { TextareaModule } from 'primeng/textarea';
-import { ChipModule } from 'primeng/chip';
+import { Component } from '@angular/core';
+import { DynamicDialogConfig } from 'primeng/dynamicdialog';
 
-import { BaseComponent } from '~/components/common/base/base.component';
-import { ThemeService } from '~/services/theme.service';
+import { DynamicField } from '~/@types';
 import { eventList } from '~/data/calendar';
-import { ACTION_DIALOG } from '~/enums';
-import { DialogHeader } from '~/pages/main/components/dialog/dialog-header/dialog-header.component';
-import { FormField } from '~/pages/main/components/dialog/form-field/form-field.component';
-import { FormAttachment } from './../../../dialog/form-attachment/form-attachment.component';
-import { ButtonPrimary } from '~/pages/main/components/shared/button-primary/button-primary.component';
+import { DynamicDialog } from '~/pages/main/components/dialog/dynamic-dialog/dynamic-dialog.component';
 
 @Component({
   selector: 'dynamic-event',
-  imports: [
-    DividerModule,
-    SelectModule,
-    FormsModule,
-    InputTextModule,
-    DatePickerModule,
-    TextareaModule,
-    ChipModule,
-    DialogHeader,
-    FormField,
-    FormAttachment,
-    ButtonPrimary
-  ],
+  imports: [DynamicDialog],
   templateUrl: './dynamic-event.component.html',
   styleUrl: './dynamic-event.component.scss'
 })
-export class DynamicEvent extends BaseComponent {
-  @ViewChild('chipInput') chipInput!: ElementRef;
+export class DynamicEvent {
+  data: any;
+  type = '';
+
   eventList = eventList;
-  dialogType = '';
   tags: string[] = [];
   inputValue: string = '';
-  isBackspacePressed = false;
-  ACTION_DIALOG = ACTION_DIALOG;
-  eventData: any;
 
-  constructor(
-    themeService: ThemeService,
-    public ref: DynamicDialogRef,
-    public config: DynamicDialogConfig
-  ) {
-    super(themeService);
-    this.dialogType = this.config.data.type;
-    this.eventData = this.config.data.event ?? {};
-  }
-
-  get isCreateMode() {
-    return this.dialogType === this.ACTION_DIALOG.CREATE;
-  }
-
-  left_box = [
+  list_columns: DynamicField[] = [
     {
       icon: 'list-sm',
       field: 'event_type',
       label: 'Event Type',
       type: 'select',
       list: this.eventList,
-      placeholder: 'Select'
+      placeholder: 'Select',
+      position: 'left'
     },
     {
       icon: 'clock',
       field: 'start_date',
       label: 'Start',
       type: 'date',
-      placeholder: 'Set date & time'
+      placeholder: 'Set date & time',
+      position: 'left'
     },
     {
       icon: 'check-circle-broken',
@@ -84,54 +46,73 @@ export class DynamicEvent extends BaseComponent {
         { name: 'Yes', code: 'Y' },
         { name: 'No', code: 'N' }
       ],
-      width: '280px'
-    }
-  ];
-
-  right_box = [
+      // width: '280px',
+      placeholder: 'Select',
+      position: 'left'
+    },
     {
       icon: 'location',
       field: 'location',
       label: 'Location',
       type: 'input',
       list: [],
-      placeholder: 'Enter location or video meeting link'
+      placeholder: 'Enter location or video meeting link',
+      position: 'right'
     },
     {
       icon: 'clock',
       field: 'end_date',
       label: 'End',
       type: 'date',
-      placeholder: 'Set date & time'
+      placeholder: 'Set date & time',
+      position: 'right'
     },
     {
       icon: 'coins-hand',
       field: 'price',
       label: 'Price',
       type: 'input',
-      placeholder: 'Leave empty for free events'
+      placeholder: 'Leave empty for free events',
+      position: 'right'
     }
   ];
 
-  closeDialog() {
-    this.ref.close();
-  }
-
-  addTag(event: any) {
-    event.preventDefault();
-
-    const newTag = this.inputValue.trim();
-    if (newTag && !this.tags.includes(newTag)) {
-      this.tags.push(newTag);
-      this.inputValue = '';
+  list_textarea = [
+    {
+      title: 'Event Description',
+      placeholder: 'Enter description',
+      value: ''
     }
+  ];
+
+  constructor(public config: DynamicDialogConfig) {
+    this.data = config.data;
+    this.type = this.data.type;
   }
 
-  removeTag(index: any) {
-    this.tags.splice(index, 1);
+  get title() {
+    return this.type === 'create' ? 'Create New Event' : 'Event Detail';
   }
 
-  focusInput() {
-    this.chipInput.nativeElement.focus();
+  get formData() {
+    return this.config.data;
   }
+
+  // addTag(event: any) {
+  //   event.preventDefault();
+
+  //   const newTag = this.inputValue.trim();
+  //   if (newTag && !this.tags.includes(newTag)) {
+  //     this.tags.push(newTag);
+  //     this.inputValue = '';
+  //   }
+  // }
+
+  // removeTag(index: any) {
+  //   this.tags.splice(index, 1);
+  // }
+
+  // focusInput() {
+  //   this.chipInput.nativeElement.focus();
+  // }
 }
