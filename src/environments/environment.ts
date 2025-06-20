1e2671e574dcb6cf4dfe5b770096c249034f6222
nhath24042001 <nhath24042001@gmail.com>
2025-06-20 10:57:45 +0700

    sync from private at 2025-06-20 10:57:45

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,6 +3,5 @@ export const environment = {
   API_URL: 'https://stg.api.hoaallinone.com/api',
   SECRET_KEY: 'HSDS',
   REST_COUNTRIES_API: 'https://restcountries.com/v3.1/all',
-  TOKEN: 'f861c83f988cd932ec5d7d49671dfec14944c7669ae08037829a709ee265a43a',
-  TEST: ''
+  TOKEN: 'f861c83f988cd932ec5d7d49671dfec14944c7669ae08037829a709ee265a43a'
 };
